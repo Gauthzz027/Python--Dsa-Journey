94bcb8ee6d4855aa1698ea21497b733e80da8864
Gauthzz027 <laxmangautham027@gmail.com>
2025-12-17 20:58:43 +0530

    Delete Day 1 directory

--- Day 1/1.py
+++
@@ -1,5 +0,0 @@
-#Print name and age
-
-name="Gautham"
-age=20
-print(f"My name is {name} and I'm {age} years old")
--- Day 1/3.py
+++
@@ -1,5 +0,0 @@
-#print area of circle
-
-r=float(input("Enter radius of the circle:"))
-area= 3.14*r*r
-print(f"The area of the circle is {area}")
