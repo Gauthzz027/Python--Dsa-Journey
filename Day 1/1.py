f80340f96d0ed50f2e99a61b9c6f23650e19cd0d
Gauthzz027 <laxmangautham027@gmail.com>
2025-12-17 20:54:55 +0530

    Add files via upload

---
+++ Day 1/1.py
@@ -0,0 +1,5 @@
+#Print name and age
+
+name="Gautham"
+age=20
+print(f"My name is {name} and I'm {age} years old")
---
+++ Day 1/2.py
@@ -0,0 +1,6 @@
+#Print sum of two numbers using user input
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=a+b
+print(f"The sum of {a} and {b} is {c}")
